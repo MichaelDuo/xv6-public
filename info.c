5a61a6b024a118f315dfc6fa0b1ac5af779328b4
MichaelDuo <michaeldong21@gmail.com>
2020-04-14 17:11:08 -0700

    add missing info and test files

---
+++ info.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char * argv[])
+{
+    if(argc<2){
+        printf(1, "Please select mode, 1, 2, 3\n");
+    } else if(argc>2){
+        printf(1, "Unsupported params, please select mode, 1, 2, 3\n");
+    } else {
+        int mode = atoi(argv[1]);
+        printf(1, "Using mode %d\n", mode);
+        info(mode);
+    }
+
+    exit();
+}
